430a2af0f0b3ff970a28387ade22109c2e7c32be
Alex Kizilov <as_k@list.ru>
2022-10-14 19:06:39 +0400

    Task2_The end

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -3,3 +3,18 @@
 а какое меньшее.
 */
 
+Console.Clear();
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+if (number1 > number2) Console.WriteLine("Первое число больше второго");
+if (number1 < number2) Console.WriteLine("Второе число больше первого");
+
+else
+{
+    if (number1 == number2)Console.WriteLine("Введенные числа одинаковые");
+}
