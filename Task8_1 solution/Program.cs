18dccbed87c4af782953f4cabd0dc501c497511b
Alex Kizilov <as_k@list.ru>
2022-10-14 22:44:33 +0400

    Add Task8_1 solution & Task8_2 solution

---
+++ Task8_1 solution/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Программа, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+*/
+
+Console.Clear();
+Console.Write("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+Console.WriteLine($"Вот все четные числа в диапазоне от " + number + " до 1" );
+Console.WriteLine();
+
+while (number > 1)
+    {
+        if (number % 2 ==0) Console.Write(number + "  " );
+        number--;
+    }
